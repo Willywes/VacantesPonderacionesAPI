e046ce4c72eb1e8160498dcccf1b0716bbf8aec2
willywes <willywes@hotmail.cl>
2018-11-09 23:29:59 -0300

    Casi detalle de Universidades

--- app/Http/Controllers/MainScraping.php
+++ app/Http/Controllers/MainScraping.php
@@ -12,26 +12,52 @@ class MainScraping extends Controller
 
     function getListColleges()
     {
-
+//prueba de vendor
 //        $crawler = Goutte::request('GET', 'https://duckduckgo.com/html/?q=Laravel');
 //        $crawler->filter('.result__title .result__a')->each(function ($node) {
 //            dump($node->text());
 //        });
-        $colleges = null;
+        $collegesList = $this->getColleges();
 
-        $crawler = Goutte::request('GET', 'http://www.psu.demre.cl/postulacion/carreras-requisitos-vacantes-y-ponderaciones-p2019/');
-        $colleges = $crawler->filter('.universidades-rendicion ul li ')->each(function ($node) {
+        $crawler = Goutte::request('GET', $collegesList[0]['link']);
 
-            return $array = [
+        $ofertas = $crawler->filter('.tabla-nomina tr')->each(function ($node) {
 
-                'image' => str_replace('../../', 'http://www.psu.demre.cl/', $node->filter('img')->attr('src')) . '',
-                'name' => $node->filter('.nombre-universidad')->text()
-
-            ];
-
+            return $results = $node->filter('td')->extract( array( '_text' ) );
 
         });
 
-        return response()->json($colleges);
+        foreach ($ofertas as $oferta){
+
+
+            if(count($oferta) == 1){
+                //return $oferta[0];
+            }
+            if(count($oferta) == 11){
+                dump ($oferta);
+            }
+        }
+
+
+        //return response()->json($ofertas);
+    }
+
+
+    private function getColleges(){
+
+        $colleges = null;
+
+        $crawler = Goutte::request('GET', 'http://www.psu.demre.cl/postulacion/carreras-requisitos-vacantes-y-ponderaciones-p2019/');
+        $colleges = $crawler->filter('.universidades-rendicion ul li')->each(function ($node) {
+            return $array = [
+
+                'image' => str_replace('../../', 'http://www.psu.demre.cl/', $node->filter('img')->attr('src')) . '',
+                'name' => $node->filter('.nombre-universidad')->text(),
+                'link' => str_replace('../', 'http://www.psu.demre.cl/postulacion/', $node->filter('.perfil-universidad a')->attr('href')),
+
+            ];
+        });
+
+        return $colleges;
     }
 }
