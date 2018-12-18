582f68e3d12e9251634907df944fd18cf1b7d257
willywes <willywes@hotmail.cl>
2018-12-18 17:44:48 -0300

    [Api Aparcate] Crear

--- app/Parking.php
+++ app/Parking.php
@@ -7,6 +7,18 @@ use Illuminate\Database\Eloquent\Model;
 class Parking extends Model
 {
 
+    protected $fillable = [
+        'id',
+        'title',
+        'content',
+        'city',
+        'period',
+        'price',
+        'available',
+        'user_id',
+        'created_at',
+        'updated_at'
+    ];
 
     public function user(){
         return $this->belongsTo(User::class);
